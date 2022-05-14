28291b4f38f7dcfbd7e6bb5a6d4d12d3bbe60c8d
cgiosy <cgiosy@kakao.com>
2022-05-14 18:55:22 +0900

    Remove unnecessary storybook configs

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,11 +1,7 @@
-const { mergeTo } = require("@cgiosy/env-react");
-
 module.exports = {
 	core: { builder: "@storybook/builder-vite" },
 	framework: "@storybook/react",
 	addons: ["@storybook/addon-essentials", "@storybook/addon-knobs"],
-	features: { storyStoreV7: true },
 	staticDirs: ["../public"],
 	stories: ["../components/**/*.stories.mdx", "../components/**/*.stories.@(jsx|tsx)"],
-	viteFinal: (config) => mergeTo(config, { resolve: { dedupe: ["@storybook/client-api"] } }),
 };
