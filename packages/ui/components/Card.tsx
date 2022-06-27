9ca59a45c0288830732aca7aa12d136f45644155
cgiosy <cgiosy@kakao.com>
2022-06-27 14:32:18 +0900

    Fix the colors of Card

--- packages/ui/components/Card.tsx
+++ packages/ui/components/Card.tsx
@@ -6,31 +6,31 @@ const cardCss = css({
 
 	display: "inline-flex",
 	flexDirection: "column",
-	background: "rgba($gray100, 0.5)",
+	background: "rgba($gray100, 0.875)",
 	border: "$$1dp solid $lowEmphasis",
 	borderRadius: "$$12dp",
 	padding: "$$16dp",
 	transition: "border 0.1s linear, background 0.1s linear, box-shadow 0.1s linear, transform 0.1s linear",
 
 	_hover: {
-		background: "rgba($gray85, 0.625)",
+		background: "rgba($gray90, 1)",
 		borderColor: "$mediumEmphasis",
 		boxShadow: "0 $$1dp $$6dp 0 rgba($gray15, 0.375)",
 	},
 	_active: {
-		background: "rgba($gray70, 0.625)",
+		background: "rgba($gray80, 1)",
 		borderColor: "$highEmphasis",
 		boxShadow: "0 $$2dp $$12dp rgba($gray15, 0.375)",
 	},
 
 	"&.filled": {
-		background: "rgba($accent80, 0.625)",
+		background: "rgba($accent85, 0.875)",
 		border: "none",
 		_hover: {
-			background: "rgba($accent70, 0.75)",
+			background: "rgba($accent80, 1)",
 			boxShadow: "none",
 		},
-		_active: { background: "rgba($accent60, 0.75)" },
+		_active: { background: "rgba($accent70, 1)" },
 	},
 
 	"&.pressable": {
